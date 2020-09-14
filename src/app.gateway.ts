172013684544332556cebfe02ccc02c6815d439e
minhthao56 <minthao5648@gmail.com>
2020-09-14 16:55:02 +0700

    add socket

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,9 +1,10 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway()
 export class AppGateway {
   @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
+  handleMessage(client: Socket, text: string): string {
     return 'Hello world!';
   }
 }
