4c4775772dfc3c3f6b4f2577b31857d90c2440f4
minhthao56 <minthao5648@gmail.com>
2020-10-13 07:55:01 +0700

    add api/v1

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+
+  app.setGlobalPrefix('api/v1');
+
   await app.listen(3000);
 }
 bootstrap();
